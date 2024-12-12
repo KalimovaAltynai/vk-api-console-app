c3fc850886937b048a4df5c85345bcc61729393d
KalimovaAltynai <kalimova@03bk.ru>
2024-12-12 20:07:25 +0500

    Изменен

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-# Замените значение access_token на ваш токеннy
+# Замените значение access_token на ваш токенннн
 access_token = 'VK_ACCESS_TOKEN'
 user_id = '169188359'  # Укажите здесь свой ID ВКонтакте
 
